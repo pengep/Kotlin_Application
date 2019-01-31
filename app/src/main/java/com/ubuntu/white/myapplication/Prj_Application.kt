2330f39121ae8fc8e47bf692234dae949ae31f18
baierpeng <baierpeng@xgrobotics.com>
2019-01-31 16:33:14 +0800

    one submission

---
+++ app/src/main/java/com/ubuntu/white/myapplication/Prj_Application.kt
@@ -0,0 +1,18 @@
+package com.ubuntu.white.myapplication
+
+import android.app.Application
+
+/**
+ * Author:baierpeng
+ * E-mail:157***4546@163.com
+ * Time:19-1-31
+ */
+class Prj_Application : Application() {
+
+
+    override fun onCreate() {
+        super.onCreate()
+
+
+    }
+}
---
+++ app/src/main/java/com/ubuntu/white/myapplication/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ubuntu.white.myapplication.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.ubuntu.white.myapplication.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
